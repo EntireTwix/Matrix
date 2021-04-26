ac4d0673d316571d62a5a761476ba0c888c8b2aa
William Katz <willkatz.dev@gmail.com>
2021-04-26 16:59:46 -0700

    Update hmat.hpp

--- hmat.hpp
+++ hmat.hpp
@@ -1,58 +1,25 @@
 #pragma once
 #include <stddef.h>
-#include <stdint.h>
-#include <concepts>
-#include <type_traits>
-
-//unsigned add long
-template <typename T>
-concept UIntegral = std::is_integral_v<T> &&std::is_unsigned_v<T>;
-
-template <UIntegral T>
-struct uadd_long : std::type_identity<T>
-{
-};
-template <>
-struct uadd_long<uint8_t> : std::type_identity<uint16_t>
-{
-};
-template <>
-struct uadd_long<uint16_t> : std::type_identity<uint32_t>
-{
-};
-template <>
-struct uadd_long<uint32_t> : std::type_identity<uint64_t>
-{
-};
-template <>
-struct uadd_long<uint64_t> : std::type_identity<uint64_t>
-{
-};
 
 template <typename T>
-using uadd_long_t = typename uadd_long<T>::type;
-
-template <typename T, UIntegral sz_t = size_t>
 class hMat
 {
 private:
     T *internal = nullptr;
-    sz_t w = 0, h = 0;
+    size_t w = 0, h = 0;
 
 public:
     using type = T;
-    using size_type = sz_t;
-    using msize_type = uadd_long_t<sz_t>; //max size type
 
     hMat() = default;
-    explicit hMat(sz_t w, sz_t h) : w(w), h(h)
+    explicit hMat(size_t w, size_t h) : w(w), h(h)
     {
         this->internal = new T[w * h]{}; //allocate and default initialize
     }
     hMat(const hMat &m) : w(m.w), h(m.h)
     {
         this->internal = new T[w * h]{}; //allocate and default initialize
-        for (msize_type i = 0; i < this->Area(); ++i)
+        for (size_t i = 0; i < this->Area(); ++i)
         {
             this->internal[i] = m.internal[i];
         }
@@ -71,14 +38,14 @@ public:
     T *data() { return internal; }
 
     //Size
-    msize_type Area() const { return Width() * Height(); }
-    sz_t Width() const { return w; }
-    sz_t Height() const { return h; }
-    void Resize(sz_t new_w, sz_t new_h)
+    size_t Area() const { return Width() * Height(); }
+    size_t Width() const { return w; }
+    size_t Height() const { return h; }
+    void Resize(size_t new_w, size_t new_h)
     {
-        msize_type new_area = new_w * new_h;
+        size_t new_area = new_w * new_h;
         T *temp = new T[new_area]{};
-        for (msize_type i = 0; i < this->Area() && i < new_area; ++i)
+        for (size_t i = 0; i < this->Area() && i < new_area; ++i)
         {
             temp[i] = this->internal[i];
         }
@@ -89,10 +56,10 @@ public:
     }
 
     //Indexing
-    T &At(size_type x, size_type y) { return FastAt((y * (Width()) + x)); }
-    T At(size_type x, size_type y) const { return FastAt((y * (Width()) + x)); }
+    T &At(size_t x, size_t y) { return FastAt((y * (Width()) + x)); }
+    T At(size_t x, size_t y) const { return FastAt((y * (Width()) + x)); }
 
-    T &FastAt(msize_type index)
+    T &FastAt(size_t index)
     {
         if (index >= this->Area())
         {
@@ -100,7 +67,7 @@ public:
         }
         return internal[index];
     }
-    T FastAt(msize_type index) const
+    T FastAt(size_t index) const
     {
         if (index >= this->Area())
         {
