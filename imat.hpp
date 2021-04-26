782366e6a17489e052ef78f952561e3ee5c73cf5
William Katz <willkatz.dev@gmail.com>
2021-04-26 14:56:50 -0700

    :bug: Fixed max size alias with Metaprogramming

--- imat.hpp
+++ imat.hpp
@@ -1,7 +1,36 @@
 #pragma once
 #include <stddef.h>
+#include <stdint.h>
 #include <concepts>
 #include <type_traits>
+
+template <typename T>
+requires std::is_unsigned_v<T> struct uadd_long : std::type_identity<T>
+{
+};
+
+template <>
+struct uadd_long<uint8_t> : std::type_identity<uint16_t>
+{
+};
+
+template <>
+struct uadd_long<uint16_t> : std::type_identity<uint32_t>
+{
+};
+
+template <>
+struct uadd_long<uint32_t> : std::type_identity<uint64_t>
+{
+};
+
+template <>
+struct uadd_long<uint64_t> : std::type_identity<uint64_t>
+{
+};
+
+template <typename T>
+using uadd_long_t = typename uadd_long<T>::type;
 
 template <typename T>
 concept Integral = std::is_integral_v<T> &&std::is_unsigned_v<T>;
@@ -12,7 +41,7 @@ class IMat
 public:
     using type = T;
     using size_type = sz_t;
-    using msize_type = decltype(std::numeric_limits<sz_t>::max() * std::numeric_limits<sz_t>::max()); //max size type
+    using msize_type = uadd_long_t<sz_t>; //max size type
 
     //Iterators
     virtual T *begin() = 0;
