ceb79fed31d43acd5822f06a72eeeb081f237fb5
William Katz <willkatz.dev@gmail.com>
2021-04-26 14:12:36 -0700

    :sparkles: Resize method

--- hmat.hpp
+++ hmat.hpp
@@ -39,6 +39,19 @@ public:
     //Size
     virtual sz_t Width() const { return w; }
     virtual sz_t Height() const { return h; }
+    void Resize(sz_t new_w, sz_t new_h)
+    {
+        typename IMat<T, sz_t>::msize_type new_area = new_w * new_h;
+        T *temp = new T[new_area]{};
+        for (typename IMat<T, sz_t>::msize_type i = 0; i < this->Area() && i < new_area; ++i)
+        {
+            temp[i] = this->internal[i];
+        }
+        w = new_w;
+        h = new_h;
+        std::swap(this->internal, temp);
+        delete[] temp;
+    }
 
     //Indexing
     virtual T &FastAt(typename IMat<T, sz_t>::msize_type index) override { return internal[index]; }
