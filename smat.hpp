528d4a47651e84f86974c4e3a25b27c30935d591
William Katz <willkatz.dev@gmail.com>
2021-04-27 09:55:02 -0700

    Update and rename include/smat.hpp to smat.hpp

---
+++ smat.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <stddef.h>
+
+template <typename T, size_t W, size_t H>
+class sMat
+{
+private:
+    T internal[H * W];
+
+public:
+    using type = T;
+
+    //Iterators
+    constexpr T *begin() { return &internal[0]; }
+    constexpr T *end() { return &internal[this->Area() - 1]; }
+    constexpr const T *begin() const { return &internal[0]; }
+    constexpr const T *end() const { return &internal[this->Area() - 1]; }
+    constexpr T *data() { return &internal[0]; }
+
+    //Size
+    constexpr size_t Area() const { return Width() * Height(); }
+    constexpr size_t Width() const { return W; }
+    constexpr size_t Height() const { return H; }
+
+    //Indexing
+    constexpr T &At(size_t x, size_t y) { return FastAt((y * (Width()) + x)); }
+    constexpr T At(size_t x, size_t y) const { return FastAt((y * (Width()) + x)); }
+
+    constexpr T &FastAt(size_t index)
+    {
+        if (index >= this->Area())
+        {
+            throw std::out_of_range("FastAt& out of range");
+        }
+        return internal[index];
+    }
+    constexpr T FastAt(size_t index) const
+    {
+        if (index >= this->Area())
+        {
+            throw std::out_of_range("FastAt out of range");
+        }
+        return internal[index];
+    }
+};
