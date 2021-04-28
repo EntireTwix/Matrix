32185ab26642835ba19bb753f8f957bd2e852dce
William Katz <willkatz.dev@gmail.com>
2021-04-27 22:08:56 -0700

    :racehorse: noexcept changes

--- smat.hpp
+++ smat.hpp
@@ -9,20 +9,20 @@ private:
     T internal[H * W]{};
 
 public:
-    sMat() = default;
+    sMat() noexcept = default;
     using type = T;
 
     //Iterators
-    constexpr T *begin() { return &internal[0]; }
-    constexpr T *end() { return &internal[this->Area() - 1]; }
-    constexpr const T *begin() const { return &internal[0]; }
-    constexpr const T *end() const { return &internal[this->Area() - 1]; }
+    constexpr T *begin() noexcept { return &internal[0]; }
+    constexpr T *end() noexcept { return &internal[this->Area() - 1]; }
+    constexpr const T *begin() const noexcept { return &internal[0]; }
+    constexpr const T *end() const noexcept { return &internal[this->Area() - 1]; }
     constexpr T *data() { return &internal[0]; }
 
     //Size
-    constexpr size_t Area() const { return Width() * Height(); }
-    constexpr size_t Width() const { return W; }
-    constexpr size_t Height() const { return H; }
+    constexpr size_t Area() const noexcept { return Width() * Height(); }
+    constexpr size_t Width() const noexcept { return W; }
+    constexpr size_t Height() const noexcept { return H; }
 
     //Indexing
     constexpr T &At(size_t x, size_t y) { return FastAt((y * (Width()) + x)); }
