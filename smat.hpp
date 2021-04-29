485ed548ff2410925a6723c9c61dba24bffe6d34
William Katz <willkatz.dev@gmail.com>
2021-04-29 16:26:02 -0700

    namespace mat

--- smat.hpp
+++ smat.hpp
@@ -23,50 +23,53 @@
 #pragma once
 #include <stddef.h>
 
-template <typename T, size_t W, size_t H>
-class sMat
+namespace mat
 {
-private:
-    T internal[H * W]{};
-
-public:
-    using type = T;
-
-    sMat() noexcept = default;
-
-    //Iterators
-    constexpr T *begin() noexcept { return &internal[0]; }
-    constexpr T *end() noexcept { return &internal[this->Area() - 1]; }
-    constexpr const T *begin() const noexcept { return &internal[0]; }
-    constexpr const T *end() const noexcept { return &internal[this->Area() - 1]; }
-    constexpr T *data() { return &internal[0]; }
-
-    //Size
-    constexpr size_t Area() const noexcept { return Width() * Height(); }
-    constexpr size_t Width() const noexcept { return W; }
-    constexpr size_t Height() const noexcept { return H; }
-
-    //Indexing
-    constexpr T &At(size_t x, size_t y) { return FastAt((y * (Width()) + x)); }
-    constexpr T At(size_t x, size_t y) const { return FastAt((y * (Width()) + x)); }
-
-    constexpr T &FastAt(size_t index)
+    template <typename T, size_t W, size_t H>
+    class sMat
     {
-        if (index >= this->Area())
-        {
-            throw std::out_of_range("FastAt& out of range");
-        }
-        return internal[index];
-    }
-    constexpr T FastAt(size_t index) const
-    {
-        if (index >= this->Area())
-        {
-            throw std::out_of_range("FastAt out of range");
-        }
-        return internal[index];
-    }
+    private:
+        T internal[H * W]{};
 
-    //Other
-    hMat SizeCopy() const noexcept { return sMat<W, H>; }
-};
+    public:
+        using type = T;
+
+        sMat() noexcept = default;
+
+        //Iterators
+        constexpr T *begin() noexcept { return &internal[0]; }
+        constexpr T *end() noexcept { return &internal[this->Area() - 1]; }
+        constexpr const T *begin() const noexcept { return &internal[0]; }
+        constexpr const T *end() const noexcept { return &internal[this->Area() - 1]; }
+        constexpr T *data() { return &internal[0]; }
+
+        //Size
+        constexpr size_t Area() const noexcept { return Width() * Height(); }
+        constexpr size_t Width() const noexcept { return W; }
+        constexpr size_t Height() const noexcept { return H; }
+
+        //Indexing
+        constexpr T &At(size_t x, size_t y) { return FastAt((y * (Width()) + x)); }
+        constexpr T At(size_t x, size_t y) const { return FastAt((y * (Width()) + x)); }
+
+        constexpr T &FastAt(size_t index)
+        {
+            if (index >= this->Area())
+            {
+                throw std::out_of_range("FastAt& out of range");
+            }
+            return internal[index];
+        }
+        constexpr T FastAt(size_t index) const
+        {
+            if (index >= this->Area())
+            {
+                throw std::out_of_range("FastAt out of range");
+            }
+            return internal[index];
+        }
+
+        //Other
+        hMat SizeCopy() const noexcept { return sMat<W, H>; }
+    };
+}
