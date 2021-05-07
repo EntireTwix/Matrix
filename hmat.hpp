ee3f20b35409081a4855829ecb0f93cc8a35f76c
William Katz <willkatz.dev@gmail.com>
2021-05-06 17:17:31 -0700

    :bug: forgot copy_fast

--- hmat.hpp
+++ hmat.hpp
@@ -25,6 +25,12 @@
 #include <stdexcept>
 #include <stddef.h>
 
+//CopyFast metaprogramming type
+template <typename T>
+struct copy_fast : std::conditional<std::is_trivially_copyable_v<T>, T, const T &> {};
+template <typename T>
+using copy_fast_t = typename copy_fast<T>::type;
+
 namespace mat
 {
     template <typename T>
