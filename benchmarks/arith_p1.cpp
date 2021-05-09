ea71044e6cbbbe5aa389124c8086549751560ddc
William Katz <willkatz.dev@gmail.com>
2021-05-09 15:41:04 -0700

    formatting

--- benchmarks/arith_p1.cpp
+++ benchmarks/arith_p1.cpp
@@ -10,24 +10,24 @@
 #include <ctime>
 #include <ratio>
 #include <chrono>
-#define time_func(f)                                                               \
-    {                                                                              \
-        using namespace std::chrono;                                               \
-        uint32_t res = 0;                                                            \
-        for (int i = 0; i < 100000; ++i)                                           \
-        {                                                                          \
-            auto t1 = high_resolution_clock::now().time_since_epoch();   \
-            f;                                                                     \
-            auto t2 = high_resolution_clock::now().time_since_epoch();   \
-            res += std::chrono::duration_cast< std::chrono::nanoseconds >( (t2-t1) ).count();                               \
-        }                                                                          \
-        std::cout << res / 100000 <<'\n';               \
+#define time_func(f)                                                                        \
+    {                                                                                       \
+        using namespace std::chrono;                                                        \
+        uint32_t res = 0;                                                                   \
+        for (int i = 0; i < 100000; ++i)                                                    \
+        {                                                                                   \
+            auto t1 = high_resolution_clock::now().time_since_epoch();                      \
+            f;                                                                              \
+            auto t2 = high_resolution_clock::now().time_since_epoch();                      \
+            res += std::chrono::duration_cast<std::chrono::nanoseconds>((t2 - t1)).count(); \
+        }                                                                                   \
+        std::cout << res / 100000 << '\n';                                                  \
     }
 
-#define Op(v, name)               \
-    {                       \
-        std::cout<<name;  \
-        time_func(v);       \
+#define Op(v, name)        \
+    {                      \
+        std::cout << name; \
+        time_func(v);      \
     }
 
 int main()
@@ -51,27 +51,26 @@ int main()
     Op(s + s, "stack + stack\n");
     Op(s + h, "stack + heap\n");
     Op(s + h, "stack + heap\n");
-    std::cout<<'\n';
+    std::cout << '\n';
 
     Op(h - h, "heap - heap\n");
     Op(h - s, "heap - stack\n");
     Op(s - s, "stack - stack\n");
     Op(s - h, "stack - heap\n");
     Op(s - h, "stack - heap\n");
-    std::cout<<'\n';
+    std::cout << '\n';
 
     Op(h * h, "heap * heap\n");
     Op(h * s, "heap * stack\n");
     Op(s * s, "stack * stack\n");
     Op(s * h, "stack * heap\n");
     Op(s * h, "stack * heap\n");
-    std::cout<<'\n';
+    std::cout << '\n';
 
     Op(h / h, "heap / heap\n");
     Op(h / s, "heap / stack\n");
     Op(s / s, "stack / stack\n");
     Op(s / h, "stack / heap\n");
     Op(s / h, "stack / heap\n");
-    std::cout<<'\n';
-    
+    std::cout << '\n';
 }
