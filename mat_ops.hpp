35c04fe1987bfa51f85116d4ee9055c4bd7414b2
William Katz <willkatz.dev@gmail.com>
2021-05-08 19:07:46 -0700

    :sparkles: Made far more generic

--- mat_ops.hpp
+++ mat_ops.hpp
@@ -47,26 +47,23 @@ namespace mat
         return os;
     }   
 
-    //Add
-    template <Matrix M, Matrix M2>
-    requires Addable<typename M::type, typename M2::type>
-    constexpr M Add(const M& a, const M2& b)
+    //Generic Operations
+    template <Matrix M, Matrix M2, typename F>
+    constexpr M Operation(const M& a, const M2& b, F&& func)
     {
         if(b.Area() < a.Area()) 
         {
             throw std::invalid_argument("arg is too big");
         }
-
         M res(a.SizeCopy());
         for(size_t i = 0; i < a.Area(); ++i)
         {
-            res.FastAt(i) = a.FastAt(i) + b.FastAt(i);
+            res.FastAt(i) = func(a.FastAt(i), b.FastAt(i));
         }
         return res;
     }
-    template <Matrix M, Matrix M2>
-    requires Addable<typename M::type, typename M2::type>
-    constexpr void AddMut(M& a, const M2& b)
+    template <Matrix M, Matrix M2, typename F>
+    constexpr void OperationMut(M& a, const M2& b, F&& func)
     {
         if(b.Area() < a.Area()) 
         {
@@ -74,179 +71,81 @@ namespace mat
         }
         for(size_t i = 0; i < a.Area(); ++i)
         {
-            a.FastAt(i) = a.FastAt(i) + b.FastAt(i);
+            a.FastAt(i) =func(a.FastAt(i), b.FastAt(i));
         }
     }
-    template <Matrix M, typename T = typename M::type>
-    requires Addable<typename M::type, T>
-    constexpr M ScalarAdd(const M& mat, copy_fast_cv_t<T> v) noexcept
+    template <Matrix M, typename F, typename T = typename M::type>
+    constexpr M ScalarOperation(const M& mat, copy_fast_cv_t<T> v, F&& func) noexcept
     {
-        std::cout<<v<<'\n';
         M res(mat.SizeCopy());
         for(size_t i = 0; i < mat.Area(); ++i)
         {
-            res.FastAt(i) = mat.FastAt(i) + v;
+            res.FastAt(i) = func(mat.FastAt(i), v);
         }
         return res;
     }
-    template <Matrix M, typename T = typename M::type>
-    requires Addable<typename M::type, T>
-    constexpr void ScalarAddMut(M& mat, copy_fast_cv_t<T> v)  noexcept
+    template <Matrix M, typename F, typename T = typename M::type>
+    constexpr void ScalarOperationMut(M& mat, copy_fast_cv_t<T> v, F&& func)  noexcept
     {
         for(T& e : mat)
         {
-            e = e + v;
+            e = func(e, v);
         }
     }
+
+    //Add
+    template <Matrix M, Matrix M2>
+    requires Addable<typename M::type, typename M2::type>
+    constexpr M Add(const M& a, const M2& b) { return Operation(a,b,[](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<typename M::type> b){ return a+b; }); }
+    template <Matrix M, Matrix M2>
+    requires Addable<typename M::type, typename M2::type>
+    constexpr void AddMut(M& a, const M2& b) { return OperationMut(a, b, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ return a+b; }); }
+    template <Matrix M, typename T = typename M::type>
+    requires Addable<typename M::type, T>
+    constexpr M ScalarAdd(const M& mat, copy_fast_cv_t<T> v) noexcept { return ScalarOperation(mat, v, [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a+b }); }
+    template <Matrix M, typename T = typename M::type>
+    requires Addable<typename M::type, T>
+    constexpr void ScalarAddMut(M& mat, copy_fast_cv_t<T> v)  noexcept { return ScalarOperationMut(mat, v, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ return a+b; }); }
 
     //Sub
     template <Matrix M, Matrix M2>
     requires Subtractable<typename M::type, typename M2::type>
-    constexpr M Sub(const M& a, const M2& b)
-    {
-        if(b.Area() < a.Area()) 
-        {
-            throw std::invalid_argument("arg is too big");
-        }
-        M res(a.SizeCopy());
-        for(size_t i = 0; i < a.Area(); ++i)
-        {
-            res.FastAt(i) = a.FastAt(i) - b.FastAt(i);
-        }
-        return res;
-    }
+    constexpr M Sub(const M& a, const M2& b) { return Operation(a,b,[](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<typename M::type> b){ return a-b; }); }
     template <Matrix M, Matrix M2>
     requires Subtractable<typename M::type, typename M2::type>
-    constexpr void SubMut(M& a, const M2& b)
-    {
-        if(b.Area() < a.Area()) 
-        {
-            throw std::invalid_argument("arg is too big");
-        }
-        for(size_t i = 0; i < a.Area(); ++i)
-        {
-            a.FastAt(i) -= b.FastAt(i);
-        }
-    }
+    constexpr void SubMut(M& a, const M2& b) { return OperationMut(a, b, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ return a-b; }); }
     template <Matrix M, typename T = typename M::type>
     requires Subtractable<typename M::type, T>
-    constexpr M ScalarSub(const M& mat, copy_fast_cv_t<T> v) noexcept
-    {
-        M res(mat.SizeCopy());
-        for(size_t i = 0; i < mat.Area(); ++i)
-        {
-            res.FastAt(i) = mat.FastAt(i) - v;
-        }
-        return res;
-    }
+    constexpr M ScalarSub(const M& mat, copy_fast_cv_t<T> v) noexcept { return ScalarOperation(mat, v, [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a-b }); }
     template <Matrix M, typename T = typename M::type>
     requires Subtractable<typename M::type, T>
-    constexpr void ScalarSubMut(M& mat, copy_fast_cv_t<T> v) noexcept
-    {
-        for(T& e : mat)
-        {
-            e -= v;
-        }
-    }
+    constexpr void ScalarSubMut(M& mat, copy_fast_cv_t<T> v)  noexcept { return ScalarOperationMut(mat, v, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ return a-b; }); }
 
     //Mul
     template <Matrix M, Matrix M2>
     requires Multipliable<typename M::type, typename M2::type>
-    constexpr M Mul(const M& a, const M2& b)
-    {
-        if(b.Area() < a.Area()) 
-        {
-            throw std::invalid_argument("arg is too big");
-        }
-        M res(a.SizeCopy());
-        for(size_t i = 0; i < a.Area(); ++i)
-        {
-            res.FastAt(i) = a.FastAt(i) * b.FastAt(i);
-        }
-        return res;
-    }
+    constexpr M Mul(const M& a, const M2& b) { return Operation(a,b,[](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<typename M::type> b){ return a*b; }); }
     template <Matrix M, Matrix M2>
     requires Multipliable<typename M::type, typename M2::type>
-    constexpr void MulMut(M& a, const M2& b)
-    {
-        if(b.Area() < a.Area()) 
-        {
-            throw std::invalid_argument("arg is too big");
-        }
-        for(size_t i = 0; i < a.Area(); ++i)
-        {
-            a.FastAt(i) *= b.FastAt(i);
-        }
-    }
+    constexpr void MulMut(M& a, const M2& b) { return OperationMut(a, b, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ return a*b; }); }
     template <Matrix M, typename T = typename M::type>
     requires Multipliable<typename M::type, T>
-    constexpr M ScalarMul(const M& mat, copy_fast_cv_t<T> v) noexcept
-    {
-        M res(mat.SizeCopcy());
-        for(size_t i = 0; i < mat.Area(); ++i)
-        {
-            res.FastAt(i) = mat.FastAt(i) * v;
-        }
-        return res;
-    }
+    constexpr M ScalarMul(const M& mat, copy_fast_cv_t<T> v) noexcept { return ScalarOperation(mat, v, [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a*b; }); }
     template <Matrix M, typename T = typename M::type>
     requires Multipliable<typename M::type, T>
-    constexpr void ScalarMulMut(M& mat, copy_fast_cv_t<T> v) noexcept
-    {
-        for(T& e : mat)
-        {
-            e *= v;
-        }
-    }
+    constexpr void ScalarMulMut(M& mat, copy_fast_cv_t<T> v)  noexcept { return ScalarOperationMut(mat, v, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ return a*b; }); }
 
     //Div
     template <Matrix M, Matrix M2>
     requires Divideable<typename M::type, typename M2::type>
-    constexpr M Div(const M& a, const M2& b)
-    {
-        if(b.Area() < a.Area()) 
-        {
-            throw std::invalid_argument("arg is too big");
-        }
-        M res(a.SizeCopy());
-        for(size_t i = 0; i < a.Area(); ++i)
-        {
-            res.FastAt(i) = a.FastAt(i) / b.FastAt(i);
-        }
-        return res;
-    }
+    constexpr M Div(const M& a, const M2& b) { return Operation(a,b,[](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<typename M::type> b){ return a/b; }); }
     template <Matrix M, Matrix M2>
     requires Divideable<typename M::type, typename M2::type>
-    constexpr void DivMut(M& a, const M2& b)
-    {
-        if(b.Area() < a.Area()) 
-        {
-            throw std::invalid_argument("arg is too big");
-        }
-        for(size_t i = 0; i < a.Area(); ++i)
-        {
-            a.FastAt(i) /= b.FastAt(i);
-        }
-    }
-    template <Matrix M, typename T>
+    constexpr void DivMut(M& a, const M2& b) { return OperationMut(a, b, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ return a/b; }); }
+    template <Matrix M, typename T = typename M::type>
     requires Divideable<typename M::type, T>
-    constexpr M ScalarDiv(const M& mat, copy_fast_cv_t<T> v) noexcept
-    {
-        M res(mat.SizeCopy());
-        for(size_t i = 0; i < mat.Area(); ++i)
-        {
-            res.FastAt(i) = mat.FastAt(i) / v;
-        }
-        return res;
-    }
-    template <Matrix M, typename T>
+    constexpr M ScalarDiv(const M& mat, copy_fast_cv_t<T> v) noexcept { return ScalarOperation(mat, v, [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a/b }); }
+    template <Matrix M, typename T = typename M::type>
     requires Divideable<typename M::type, T>
-    constexpr void ScalarDivMut(M& mat, copy_fast_cv_t<T> v) noexcept
-    {
-        for(T& e : mat)
-        {
-            e /= v;
-        }
-    }
-
+    constexpr void ScalarAddMut(M& mat, copy_fast_cv_t<T> v)  noexcept { return ScalarOperationMut(mat, v, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ return a/b; }); }
 };
