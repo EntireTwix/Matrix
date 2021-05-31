eb1c03d5fb3c64b761fa598adabca066e30257d8
EntireTwix <willkatz.dev@gmail.com>
2021-05-31 00:41:31 -0700

    non C++20 variant of is_constexpr_matrix for if constexpr conditionals

---
+++ operations/std/11+/is_constexpr_mat.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+template <auto E>
+struct has_constexpr_element : std::integral_constant<decltype(E), E> {};
+
+template <typename M>
+using is_constexpr_matrix = has_constexpr_element<M::Area>;
+template <typename T>
+using is_constexpr_matrix_v = is_constexpr_matrix<T>::value;
