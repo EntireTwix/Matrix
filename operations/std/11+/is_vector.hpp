d4ced9893a7b64b0a3680ff89b743479c8ab1e13
EntireTwix <willkatz.dev@gmail.com>
2021-05-31 00:40:05 -0700

    :sparkles: is_vector non-C++20 variant

--- operations/std/11+/is_vector.hpp
+++ operations/std/11+/is_vector.hpp
@@ -1,10 +1,18 @@
 #pragma once
+#include "is_constexpr_matrix.hpp"
 
 namespace mat
 {
     template <typename M>
     void IsVector(const M &mat)
     {
-        assert(mat.Width() == 1 || mat.Height() == 1);
+        if constexpr(is_constexpr_matrix_v<M>)
+        {
+            static_assert(mat.Width() == 1 || mat.Height() == 1);
+        }
+        else
+        {
+            assert(mat.Width() == 1 || mat.Height() == 1);
+        }
     }
 };
