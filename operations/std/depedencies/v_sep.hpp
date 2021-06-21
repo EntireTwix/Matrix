e0fa09f04bfe06f3c35ff86e3d20b517d86b828e
EntireTwix <willkatz.dev@gmail.com>
2021-06-21 16:35:06 -0700

    simplified

--- operations/std/depedencies/v_sep.hpp
+++ operations/std/depedencies/v_sep.hpp
@@ -7,26 +7,11 @@
 #endif
 
 #ifdef __cpp_concepts 
-
 #define EXEC_IF_20_ELSE(expr, expr2) expr
 #else
 #define EXEC_IF_20_ELSE(expr, expr2) expr2
 #endif
 
-#ifdef __cpp_concepts  
-#define MATRIX_TYPENAME Matrix
-#else
-#define MATRIX_TYPENAME typename
-#endif
-
-#ifdef __cpp_concepts  
-#define RUNTIME_MATRIX_TYPENAME RuntimeMatrix
-#else
-#define RUNTIME_MATRIX_TYPENAME typename
-#endif
-
-#ifdef __cpp_concepts  
-#define CONSTEXPR_MATRIX_TYPENAME ConstexprMatrix
-#else
-#define CONSTEXPR_MATRIX_TYPENAME typename
-#endif
+#define MATRIX_TYPENAME EXEC_IF_20_ELSE(Matrix, typename)
+#define RUNTIME_MATRIX_TYPENAME EXEC_IF_20_ELSE(RuntimeMatrix, typename)
+#define CONSTEXPR_MATRIX_TYPENAME EXEC_IF_20_ELSE(ConstexprMatrix, typename)
