0406a04670b70a60f7ea9e2d5be320a1c23131ed
EntireTwix <willkatz.dev@gmail.com>
2021-06-21 17:16:05 -0700

    :racehorse: passed value for scalar ops as move

--- operations/std/arithmetic_ops.hpp
+++ operations/std/arithmetic_ops.hpp
@@ -38,12 +38,12 @@ namespace mat
     #ifdef __cpp_concepts 
     requires Addable<typename M::type, T>
     #endif
-    constexpr M operator+(const M& mat, T&& v) noexcept { return ScalarOperation(mat, v, [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a+b; }); }
+    constexpr M operator+(const M& mat, T&& v) noexcept { return ScalarOperation(mat, std::move(v), [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a+b; }); }
     template <MATRIX_TYPENAME M, typename T = typename M::type>
     #ifdef __cpp_concepts 
     requires AddableAs<typename M::type, T>
     #endif
-    constexpr void operator+=(M& mat, T&& v)  noexcept { ScalarOperationMut(mat, v, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ a+=b; }); }
+    constexpr void operator+=(M& mat, T&& v)  noexcept { ScalarOperationMut(mat, std::move(v), [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ a+=b; }); }
 
     //Sub
     template <MATRIX_TYPENAME M, MATRIX_TYPENAME M2>
@@ -60,12 +60,12 @@ namespace mat
     #ifdef __cpp_concepts 
     requires Subtractable<typename M::type, T>
     #endif
-    constexpr M operator-(const M& mat, T&& v) noexcept { return ScalarOperation(mat, v, [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a-b; }); }
+    constexpr M operator-(const M& mat, T&& v) noexcept { return ScalarOperation(mat, std::move(v), [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a-b; }); }
     template <MATRIX_TYPENAME M, typename T = typename M::type>
     #ifdef __cpp_concepts 
     requires SubtractableAs<typename M::type, T>
     #endif
-    constexpr void operator-=(M& mat, T&& v)  noexcept { ScalarOperationMut(mat, v, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ a-=b; }); }
+    constexpr void operator-=(M& mat, T&& v)  noexcept { ScalarOperationMut(mat, std::move(v), [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ a-=b; }); }
 
     //Mul
     template <MATRIX_TYPENAME M, MATRIX_TYPENAME M2>
@@ -82,12 +82,12 @@ namespace mat
     #ifdef __cpp_concepts 
     requires Multipliable<typename M::type, T>
     #endif
-    constexpr M operator*(const M& mat, T&& v) noexcept { return ScalarOperation(mat, v, [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a*b; }); }
+    constexpr M operator*(const M& mat, T&& v) noexcept { return ScalarOperation(mat, std::move(v), [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a*b; }); }
     template <MATRIX_TYPENAME M, typename T = typename M::type>
     #ifdef __cpp_concepts 
     requires MultipliableAs<typename M::type, T>
     #endif
-    constexpr void operator*=(M& mat, T&& v)  noexcept { ScalarOperationMut(mat, v, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ a*=b; }); }
+    constexpr void operator*=(M& mat, T&& v)  noexcept { ScalarOperationMut(mat, std::move(v), [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ a*=b; }); }
 
     //Div
     template <MATRIX_TYPENAME M, MATRIX_TYPENAME M2>
@@ -104,10 +104,10 @@ namespace mat
     #ifdef __cpp_concepts 
     requires Divideable<typename M::type, T>
     #endif
-    constexpr M operator/(const M& mat, T&& v) noexcept { return ScalarOperation(mat, v, [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a/b; }); }
+    constexpr M operator/(const M& mat, T&& v) noexcept { return ScalarOperation(mat, std::move(v), [](copy_fast_cv_t<typename M::type> a, copy_fast_cv_t<T> b){ return a/b; }); }
     template <MATRIX_TYPENAME M, typename T = typename M::type>
     #ifdef __cpp_concepts 
     requires DivideableAs<typename M::type, T>
     #endif
-    constexpr void operator/=(M& mat, T&& v)  noexcept { ScalarOperationMut(mat, v, [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ a/=b; }); }
+    constexpr void operator/=(M& mat, T&& v)  noexcept { ScalarOperationMut(mat, std::move(v), [](typename M::type& a, copy_fast_cv_t<typename M::type> b){ a/=b; }); }
 };
