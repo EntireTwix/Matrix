cb393f97e8b9ca2e16c599df0776f39a4809c460
EntireTwix <willkatz.dev@gmail.com>
2021-07-25 23:36:32 -0700

    :zap: made compatable with C++17

--- operations/std/depedencies/copy_fast.hpp
+++ operations/std/depedencies/copy_fast.hpp
@@ -1,9 +1,11 @@
 #pragma once
+#include <type_traits>
+
 
 //CopyFast metaprogramming type
 template <typename T>
 struct copy_fast : std::conditional<std::is_trivially_copyable_v<T>, T, const T &>{};
 template <typename T>
-struct copy_fast_cv : copy_fast<typename std::remove_cvref_t<T>>{};
+struct copy_fast_cv : copy_fast<std::remove_cv_t<std::remove_reference_t<T>>>{};
 template <typename T>
 using copy_fast_cv_t = typename copy_fast_cv<T>::type;
