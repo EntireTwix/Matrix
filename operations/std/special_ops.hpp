27069cbeac629ce25a246dccdbc49a325a87664a
EntireTwix <willkatz.dev@gmail.com>
2021-07-25 22:48:21 -0700

    :fire::zap: removing legacy #ifdef __cpp_concepts

--- operations/std/special_ops.hpp
+++ operations/std/special_ops.hpp
@@ -1,23 +1,24 @@
 #pragma once
-#include <iostream>
-
-#ifdef __cpp_concepts 
 #include "depedencies/cmat.hpp"
 
+#ifdef HAS_CONCEPTS 
+#include <iostream>
 template <typename T>
 concept Printable = requires(T a)
 {
     {std::cout << a} -> std::same_as<std::ostream &>;
 };
+#else
+#include <ostream>
 #endif
 
 namespace mat
 {
     template <MATRIX_TYPENAME M>
-    #ifdef __cpp_concepts 
+    #ifdef HAS_CONCEPTS 
     requires Printable<typename M::type>
     #endif
-    std::ostream &operator<<(std::ostream &os, const M &mat)
+    std::ostream &Print(std::ostream &os, const M &mat)
     {
         for (size_t i = 0; i < mat.Height(); ++i)
         {
