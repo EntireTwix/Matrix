9be08107b2cb3192dcb20afa8d741f4a988ce430
EntireTwix <willkatz.dev@gmail.com>
2021-07-25 20:35:27 -0700

    :art: conformed to C++ style guide

--- smat.hpp
+++ smat.hpp
@@ -8,29 +8,28 @@ namespace mat
     class sMat
     {
     private:
-        T internal[H * W]{};
+        T _internal[H * W]{};
 
     public:
         using type = T;
-        
+
         template <size_t W2, size_t H2>
         using base = sMat<T, W2, H2>;
-        
+
         static constexpr size_t width = W;
         static constexpr size_t height = H;
-        static constexpr size_t area = W*H;
 
         sMat() noexcept = default;
 
         //Iterators
-        constexpr T *begin() noexcept { return &internal[0]; }
-        constexpr T *end() noexcept { return &internal[this->Area()]; }
-        constexpr const T *begin() const noexcept { return &internal[0]; }
-        constexpr const T *end() const noexcept { return &internal[this->Area()]; }
-        constexpr T *data() { return &internal[0]; }
+        constexpr T *begin() noexcept { return &this->_internal[0]; }
+        constexpr T *end() noexcept { return &this->_internal[this->Area()]; }
+        constexpr const T *begin() const noexcept { return &this->_internal[0]; }
+        constexpr const T *end() const noexcept { return &this->_internal[this->Area()]; }
+        constexpr T *data() { return &this->_internal[0]; }
 
         //Size
-        constexpr size_t Area() const noexcept { return area; }
+        constexpr size_t Area() const noexcept { return W * H; }
         constexpr size_t Width() const noexcept { return W; }
         constexpr size_t Height() const noexcept { return H; }
 
@@ -44,7 +43,7 @@ namespace mat
             {
                 throw std::out_of_range("FastAt& out of range");
             }
-            return internal[index];
+            return this->_internal[index];
         }
         constexpr T FastAt(size_t index) const
         {
@@ -52,7 +51,7 @@ namespace mat
             {
                 throw std::out_of_range("FastAt out of range");
             }
-            return internal[index];
+            return this->_internal[index];
         }
     };
 }
