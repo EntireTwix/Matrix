c8335ff2b9b6c29c4b6dd33f8e7d72aaa9f7f678
EntireTwix <willkatz.dev@gmail.com>
2021-07-31 01:56:52 -0700

    :sparkles: capacity functionality to reduce allocs

--- hmat.hpp
+++ hmat.hpp
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-
 #pragma once
 #include <cstring>
 #include <stdexcept>
@@ -33,54 +32,62 @@ namespace mat
     {
     private:
         T *_internal = nullptr;
-        size_t _w = 0, _h = 0;
+        size_t _w = 0, _h = 0, _size = 0, _capacity = 0;
 
     public:
         using type = T;
 
         hMat() noexcept = default;
-        explicit hMat(size_t w, size_t h) noexcept : _w(w), _h(h)
+        explicit hMat(size_t w, size_t h) noexcept : _w(w), _h(h), _size(w * h), _capacity(w * h)
         {
-            this->_internal = new T[w * h]{}; //allocate and default initialize
+            this->_internal = new T[this->_size]{}; //allocate and default initialize
         }
-        hMat(const hMat &m) noexcept : _w(m._w), _h(m._h)
+        hMat(const hMat &m) noexcept : _w(m._w), _h(m._h), _size(m._size)
         {
-            this->_internal = new T[_w * _h]{}; //allocate and default initialize
+            if (m._size > this->_capacity)
+            {
+                this->_internal = new T[this->_size]; //allocate
+                this->_capacity = m->_size;
+            }
             memcpy(this->_internal, m._internal, this->Area() * sizeof(T));
         }
         hMat &operator=(const hMat &m) noexcept
         {
-            size_t area = Area();
+            size_t area = this->_size;
             this->_w = m._w;
             this->_h = m._h;
-            if (area != m.Area())
+            this->_size = m._size;
+            if (m._size > this->_capacity)
             {
-                reallocarray(this->_internal, (_w * _h), sizeof(T)); //allocate
+                this->_internal = (T*)reallocarray(this->_internal, m._size, sizeof(T)); //allocate
+                this->_capacity = m._size;
             }
-            memcpy(this->_internal, m._internal, this->Area() * sizeof(T));
+            memcpy(this->_internal, m._internal, m._size * sizeof(T));
             return *this;
         }
-        hMat(hMat &&m) noexcept : _w(m._w), _h(m._h)
+        hMat(hMat &&m) noexcept : _w(m._w), _h(m._h), _size(m._size), _capacity(m._capacity)
         {
             this->_internal = m._internal; //moving
 
             //clean up
             m._internal = nullptr;
-            m._w = m._h = 0;
+            m._w = m._h = m._size = m._capacity = 0;
         }
         hMat &operator=(hMat &&m) noexcept
         {
-            if (_internal)
+            if (this->_internal)
             {
                 delete[] _internal;
             }
             this->_w = m._w;
             this->_h = m._h;
+            this->_size = m._size;
+            this->_capacity = m._capacity;
             this->_internal = m._internal; //moving
 
             //clean up
             m._internal = nullptr;
-            m._w = m._h = 0;
+            m._w = m._h = m._size = m._capacity = 0;
             return *this;
         }
 
@@ -92,17 +99,64 @@ namespace mat
         T *data() noexcept { return this->_internal; }
 
         //Size
-        size_t Area() const noexcept { return _w * _h; }
         size_t Width() const noexcept { return _w; }
         size_t Height() const noexcept { return _h; }
+        size_t Area() const noexcept { return _size; }
+        size_t Capacity() const noexcept { return _capacity; }
+        
+        void Resize(size_t w, size_t h) noexcept
+        {
+            if (this->_w == w && this->_h == h)
+            {
+                return;
+            }
+            
+            this->_size = w * h;
+            if(this->_size > this->_capacity) 
+            {
+                //is larger
+                this->_internal = (T*)reallocarray(this->_internal, this->_size, sizeof(T)); //allocate
+                this->_capacity = this->_size;
+                for (size_t i = h; i > 0; --i)
+                {
+                    for (size_t j = w; j > 0; --j)
+                    {
+                        if (j-1 < this->_w && i-1 < this->_h)
+                        {
+                            this->_internal[((i-1) * w) + (j-1)] = ((i-1) * this->_w) + (j-1); //copy
+                        }
+                        else
+                        {
+                            this->_internal[((i-1) * w) + (j-1)] = T(); //init 
+                        }
+                    }
+                }
+            }
+            else
+            {
+                //is smaller
+                for (size_t i = 0; i < h; ++i)
+                {
+                    for (size_t j = 0; j < w; ++j)
+                    {
+                        if (j < this->_w && i < this->_h)
+                        {
+                            this->_internal[(i * w) + j] = this->_internal[(i * this->_w) + j];
+                        }
+                    }
+                }
+            }
+            this->_w = w;
+            this->_h = h;
+        }
 
         //Indexing
-        T &At(size_t x, size_t y) { return FastAt((y * (Width()) + x)); }
-        T At(size_t x, size_t y) const { return FastAt((y * (Width()) + x)); }
+        T &At(size_t x, size_t y) { return FastAt( (y * this->_w()) + x); }
+        T At(size_t x, size_t y) const { return FastAt( (y * this->_w) + x); }
 
         T &FastAt(size_t index)
         {
-            if (index >= this->Area())
+            if (index >= this->_size)
             {
                 throw std::out_of_range("FastAt& out of range");
             }
@@ -110,7 +164,7 @@ namespace mat
         }
         T FastAt(size_t index) const
         {
-            if (index >= this->Area())
+            if (index >= this->_size)
             {
                 throw std::out_of_range("FastAt out of range");
             }
