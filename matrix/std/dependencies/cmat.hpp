5a32ecc4e7d32415aa392a3b4e5be4d12d4c63a7
EntireTwix <willkatz.dev@gmail.com>
2022-01-03 13:52:52 -0800

    :bug: `copy_fast_t` was destroying intended references

--- matrix/std/dependencies/cmat.hpp
+++ matrix/std/dependencies/cmat.hpp
@@ -1,33 +1,33 @@
 #pragma once
 #include <type_traits>
 #include <stddef.h>
-#include "matrix/std/dependencies/copy_fast.hpp"
+#include "copy_fast.hpp"
 
 #ifdef __cpp_concepts
-    #if __cplusplus >= __cpp_concepts
-    #define HAS_CONCEPTS true
-    #endif
+#if __cplusplus >= __cpp_concepts
+#define HAS_CONCEPTS true
+#endif
 #endif
 
-#ifdef HAS_CONCEPTS 
+#ifdef HAS_CONCEPTS
 #define EXEC_IF_20(expr) expr
 #else
-#define EXEC_IF_20(expr) 
+#define EXEC_IF_20(expr)
 #endif
 
-#ifdef HAS_CONCEPTS 
+#ifdef HAS_CONCEPTS
 #define EXEC_IF_20_ELSE(expr, expr2) expr
 #else
 #define EXEC_IF_20_ELSE(expr, expr2) expr2
 #endif
 
-#ifdef HAS_CONCEPTS 
-#define EXEC_IF_NOT_20(expr) 
+#ifdef HAS_CONCEPTS
+#define EXEC_IF_NOT_20(expr)
 #else
 #define EXEC_IF_NOT_20(expr) expr
 #endif
 
-#ifdef HAS_CONCEPTS 
+#ifdef HAS_CONCEPTS
 #define EXEC_IF_NOT_20_ELSE(expr, expr2) expr2
 #else
 #define EXEC_IF_NOT_20_ELSE(expr, expr2) expr
@@ -40,48 +40,89 @@
 #ifdef HAS_CONCEPTS
 namespace mat
 {
-    #include <concepts>
+#include <concepts>
 
     template <typename T>
-    concept Matrix = requires(T a, const T b) {
+    concept Matrix = requires(T a, const T b)
+    {
         typename T::type;
-    
-        { a.begin() } -> std::same_as<typename T::type*>;
-        { a.end() } -> std::same_as<typename T::type*>;
-        { b.begin() } -> std::same_as<const typename T::type*>;
-        { b.end() } -> std::same_as<const typename T::type*>;
-        { a.data() } -> std::same_as<typename T::type*>;
 
-        { a.Area() } -> std::same_as<size_t>;
-        { a.Width() } -> std::same_as<size_t>;
-        { a.Height() } -> std::same_as<size_t>;
+        {
+            a.begin()
+            } -> std::same_as<typename T::type *>;
+        {
+            a.end()
+            } -> std::same_as<typename T::type *>;
+        {
+            b.begin()
+            } -> std::same_as<const typename T::type *>;
+        {
+            b.end()
+            } -> std::same_as<const typename T::type *>;
+        {
+            a.data()
+            } -> std::same_as<typename T::type *>;
 
-        { a.At(0,0) } -> std::convertible_to<typename T::type&>;
-        { a.At(0,0) } -> std::convertible_to<copy_fast_cv_t<typename T::type>>;
-        { a.FastAt(0) } -> std::convertible_to<typename T::type&>;
-        { a.FastAt(0) } -> std::convertible_to<copy_fast_cv_t<typename T::type>>;
+        {
+            a.Area()
+            } -> std::same_as<size_t>;
+        {
+            a.Width()
+            } -> std::same_as<size_t>;
+        {
+            a.Height()
+            } -> std::same_as<size_t>;
+
+        {
+            a.At(0, 0)
+            } -> std::convertible_to<typename T::type &>;
+        {
+            a.At(0, 0)
+            } -> std::convertible_to<copy_fast_t<typename T::type>>;
+        {
+            a.FastAt(0)
+            } -> std::convertible_to<typename T::type &>;
+        {
+            a.FastAt(0)
+            } -> std::convertible_to<copy_fast_t<typename T::type>>;
     };
 
     template <typename M>
-    concept ConstexprMatrix = Matrix<M> && requires(M a) {
+    concept ConstexprMatrix = Matrix<M> && requires(M a)
+    {
         typename M::base<0, 0>;
 
-        { M::area } -> std::convertible_to<size_t>;
-        { M::width } -> std::convertible_to<size_t>;
-        { M::height } -> std::convertible_to<size_t>;
+        {
+            M::area
+            } -> std::convertible_to<size_t>;
+        {
+            M::width
+            } -> std::convertible_to<size_t>;
+        {
+            M::height
+            } -> std::convertible_to<size_t>;
     };
 
     template <typename M>
-    concept RuntimeMatrix = Matrix<M> && requires(M a) {
-        { a.Flatten() } -> std::same_as<void>;
-        { a.Capacity() } -> std::same_as<size_t>;
-        { a.Resize(0, 0) } -> std::same_as<void>;
-        { a.Reserve(0) } -> std::same_as<void>;
+    concept RuntimeMatrix = Matrix<M> && requires(M a)
+    {
+        {
+            a.Flatten()
+            } -> std::same_as<void>;
+        {
+            a.Capacity()
+            } -> std::same_as<size_t>;
+        {
+            a.Resize(0, 0)
+            } -> std::same_as<void>;
+        {
+            a.Reserve(0)
+            } -> std::same_as<void>;
     };
 }
 
-    #define CONSTEXPR_MATRIX(T) mat::ConstexprMatrix<T>
-    #define RUNTIME_MATRIX(T) mat::RuntimeMatrix<T>
+#define CONSTEXPR_MATRIX(T) mat::ConstexprMatrix<T>
+#define RUNTIME_MATRIX(T) mat::RuntimeMatrix<T>
 #else
 #define HAS_(member, member_type)                                         \
     template <typename T, typename = member_type>                         \
@@ -91,10 +132,10 @@ namespace mat
     template <typename T>                                                 \
     struct has_##member<T, decltype((void)T::member, 0)> : std::true_type \
     {                                                                     \
-    };    
-    HAS_(area, size_t);
-    HAS_(width, size_t);
-    HAS_(height, size_t);
+    };
+HAS_(area, size_t);
+HAS_(width, size_t);
+HAS_(height, size_t);
 
 #define HAS_FUNC_(func_name)                                                                                                                  \
     template <typename, typename T>                                                                                                           \
@@ -111,13 +152,14 @@ namespace mat
         template <typename>                                                                                                                   \
         static constexpr std::false_type check(...);                                                                                          \
         typedef decltype(check<C>(0)) type;                                                                                                   \
+                                                                                                                                              \
     public:                                                                                                                                   \
         static constexpr bool value = type::value;                                                                                            \
-    };    
-    HAS_FUNC_(Flatten);
-    HAS_FUNC_(Capacity);
-    HAS_FUNC_(Resize);
-    HAS_FUNC_(Reserve);
+    };
+HAS_FUNC_(Flatten);
+HAS_FUNC_(Capacity);
+HAS_FUNC_(Resize);
+HAS_FUNC_(Reserve);
 
 #define HAS_STATIC_FUNC(static_method_name)                                                                                             \
     template <typename, typename T>                                                                                                     \
@@ -135,16 +177,20 @@ namespace mat
         static constexpr bool value = decltype(check<C>(nullptr))::value;                                                               \
     };
 
-    template <typename T>
-    struct is_constexpr_matrix : std::integral_constant<bool, has_area<T>::value && has_width<T>::value && has_height<T>::value> {};
-    template <typename T>
-    constexpr bool is_constexpr_matrix_v = is_constexpr_matrix<T>::value;
-    
-    template <typename T>
-    struct is_runtime_matrix : std::integral_constant<bool, has_Flatten<T, void()>::value && has_Capacity<T, size_t()>::value && has_Resize<T, void(size_t, size_t)>::value> {};
-    template <typename T>
-    constexpr bool is_runtime_matrix_v = is_constexpr_matrix<T>::value;
+template <typename T>
+struct is_constexpr_matrix : std::integral_constant<bool, has_area<T>::value && has_width<T>::value && has_height<T>::value>
+{
+};
+template <typename T>
+constexpr bool is_constexpr_matrix_v = is_constexpr_matrix<T>::value;
 
-    #define CONSTEXPR_MATRIX(T) is_constexpr_matrix_v<T>
-    #define RUNTIME_MATRIX(T) is_runtime_matrix_v<T>
+template <typename T>
+struct is_runtime_matrix : std::integral_constant<bool, has_Flatten<T, void()>::value && has_Capacity<T, size_t()>::value && has_Resize<T, void(size_t, size_t)>::value>
+{
+};
+template <typename T>
+constexpr bool is_runtime_matrix_v = is_constexpr_matrix<T>::value;
+
+#define CONSTEXPR_MATRIX(T) is_constexpr_matrix_v<T>
+#define RUNTIME_MATRIX(T) is_runtime_matrix_v<T>
 #endif
--- matrix/std/dependencies/copy_fast.hpp
+++ matrix/std/dependencies/copy_fast.hpp
@@ -6,6 +6,4 @@
 template <typename T>
 struct copy_fast : std::conditional<std::is_trivially_copyable_v<T>, T, const T &>{};
 template <typename T>
-struct copy_fast_cv : copy_fast<std::remove_cv_t<std::remove_reference_t<T>>>{};
-template <typename T>
-using copy_fast_cv_t = typename copy_fast_cv<T>::type;
+using copy_fast_t = typename copy_fast<T>::type;
