92e28cdf56d5ec9bccb6df591cd53ecb83f54b77
EntireTwix <willkatz.dev@gmail.com>
2022-03-17 11:46:01 -0700

    :bug: mat namespace not in macro gen

--- dependencies/cmat.hpp
+++ dependencies/cmat.hpp
@@ -33,9 +33,9 @@
 #define EXEC_IF_NOT_20_ELSE(expr, expr2) expr
 #endif
 
-#define MATRIX_TYPENAME EXEC_IF_20_ELSE(Matrix, typename)
-#define RUNTIME_MATRIX_TYPENAME EXEC_IF_20_ELSE(RuntimeMatrix, typename)
-#define CONSTEXPR_MATRIX_TYPENAME EXEC_IF_20_ELSE(ConstexprMatrix, typename)
+#define MATRIX_TYPENAME EXEC_IF_20_ELSE(mat::Matrix, typename)
+#define RUNTIME_MATRIX_TYPENAME EXEC_IF_20_ELSE(mat::RuntimeMatrix, typename)
+#define CONSTEXPR_MATRIX_TYPENAME EXEC_IF_20_ELSE(mat::ConstexprMatrix, typename)
 
 #ifdef HAS_CONCEPTS
 namespace mat
