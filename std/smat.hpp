ef50b4caf0d81b6404028f27d6642d4e44e5678d
EntireTwix <willkatz.dev@gmail.com>
2022-03-19 04:26:45 -0700

    :fire: removed excplicit from constructor

--- std/smat.hpp
+++ std/smat.hpp
@@ -47,7 +47,7 @@ namespace mat
         // Constructors
         constexpr sMat() noexcept = default;
         template <typename... Args>
-        constexpr explicit sMat(Args... list) : _internal{list...} {}
+        constexpr sMat(Args... list) : _internal{list...} {}
 
         //Iterators
         constexpr T *begin() noexcept { return &this->_internal[0]; }
