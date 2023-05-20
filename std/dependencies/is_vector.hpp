d55af589e63867b5c18592d212b38b55407c110d
EntireTwix <willkatz.dev@gmail.com>
2023-05-19 21:49:06 -0700

    spelling mistake in last commit

--- std/dependencies/is_vector.hpp
+++ std/dependencies/is_vector.hpp
@@ -5,9 +5,9 @@
 namespace mat
 {
     template <MATRIX_TYPENAME M>
-    constexpr void is_verctor(const M &mat)
+    constexpr void is_vector(const M &mat)
     {
-        EXEC_IF_NOT_20(static_assert(CONSTEXPR_MATRIX(M) || RUNTIME_MATRIX(M), "is_verctor: M must be a CONSTEXPR_MATRIX or RUNTIME_MATRIX"));
+        EXEC_IF_NOT_20(static_assert(CONSTEXPR_MATRIX(M) || RUNTIME_MATRIX(M), "is_vector: M must be a CONSTEXPR_MATRIX or RUNTIME_MATRIX"));
         if constexpr (CONSTEXPR_MATRIX(M))
         {
             static_assert(mat.width() == 1 || mat.height() == 1);
